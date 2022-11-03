1b89d3816f6fd9673c7e4a62ad8e0818d3a3a6d6
ullah <sheikholeei97@gmail.ocm>
2022-11-03 23:31:27 +0600

    pagination button setup done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,20 +6,23 @@ import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
-    const products = useLoaderData();
+    const { products, count } = useLoaderData();
+    const [currentPage, setCurrentPage] = useState(0);
+    const [dataPerpage, setDataPerpage] = useState(10);
+    const totalPages = Math.ceil(count / dataPerpage);
     const [cart, setCart] = useState([]);
 
-    const clearCart = () =>{
+    const clearCart = () => {
         setCart([]);
         deleteShoppingCart();
     }
 
-    useEffect( () =>{
+    useEffect(() => {
         const storedCart = getStoredCart();
         const savedCart = [];
-        for(const id in storedCart){
+        for (const id in storedCart) {
             const addedProduct = products.find(product => product._id === id);
-            if(addedProduct){
+            if (addedProduct) {
                 const quantity = storedCart[id];
                 addedProduct.quantity = quantity;
                 savedCart.push(addedProduct);
@@ -28,15 +31,15 @@ const Shop = () => {
         setCart(savedCart);
     }, [products])
 
-    const handleAddToCart = (selectedProduct) =>{
+    const handleAddToCart = (selectedProduct) => {
         console.log(selectedProduct);
         let newCart = [];
         const exists = cart.find(product => product._id === selectedProduct._id);
-        if(!exists){
+        if (!exists) {
             selectedProduct.quantity = 1;
             newCart = [...cart, selectedProduct];
         }
-        else{
+        else {
             const rest = cart.filter(product => product._id !== selectedProduct._id);
             exists.quantity = exists.quantity + 1;
             newCart = [...rest, exists];
@@ -48,14 +51,28 @@ const Shop = () => {
 
     return (
         <div className='shop-container'>
-            <div className="products-container">
-                {
-                    products.map(product=><Product
-                        key={product._id}
-                        product={product}
-                        handleAddToCart={handleAddToCart}
+            <div>
+                <div className="products-container">
+                    {
+                        products.map(product => <Product
+                            key={product._id}
+                            product={product}
+                            handleAddToCart={handleAddToCart}
                         ></Product>)
-                }
+                    }
+                </div>
+                <div className='center'>
+                    <button onClick={() => setCurrentPage(currentPage-1)} disabled={currentPage === 0 && true}>Prev</button>
+                    {
+                        [...Array(totalPages).keys()].map(num => (
+                            <button
+                            onClick={() => setCurrentPage(num)}
+                            className={currentPage === num && 'currentPages'}
+                            key={num}>{num}</button>
+                        ))
+                    }
+                    <button onClick={() => setCurrentPage(currentPage+1)} disabled={currentPage === totalPages-1 && true}>Next</button>
+                </div>
             </div>
             <div className="cart-container">
                 <Cart clearCart={clearCart} cart={cart}>
